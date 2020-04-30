7cceb9bfe6357cf1e38d7bb36ade4f03809b262d
Maciej Gierwatowski <maciej.gierwatowski@gmail.com>
2020-04-30 18:04:36 +0200

    mx_tools

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,11 @@ output "json_map" {
   description = "JSON encoded container definitions for use with other terraform resources such as aws_ecs_task_definition"
   value       = local.json_map
 }
+
+output "object" {
+  value = local.container_definition
+}
+
+output "container_name" {
+  value = local.name
+}
--- variables.tf
+++ variables.tf
@@ -1,105 +1,222 @@
 variable "container_definition" {
-  type = object({
-    command = string
-    cpu = number
-    dependsOn = string
-    dnsServers = string
-    dockerLabels = string
-    entryPoint = string
-    environment = list(object({
-      name = string
-      value = string
-    }))
-    essential = string
-    firelensConfiguration = string
-    healthCheck = string
-    image = string
-    links = string
-    logConfiguration = string
-    memory = number
-    memoryReservation = number
-    mountPoints = string
-    name = string
-    portMappings = list(object({
-      containerPort = number
-      hostPort = number
-      protocol = string
-    }))
-    privileged = string
-    readonlyRootFilesystem = bool
-    repositoryCredentials = string
-    secrets = string
-    startTimeout = number
-    stopTimeout = number
-    systemControls = string
-    ulimits = string
-    user = string
-    volumesFrom = string
-    workingDirectory = string
-  })
   description = "Original container definition"
-  default = {
-    command = null
-    cpu = 256
-    dependsOn = null
-    dnsServers = null
-    dockerLabels = null
-    entryPoint = null
-    environment = []
-    essential = true
-    firelensConfiguration = null
-    healthCheck = null
-    image = mull
-    links = null
-    logConfiguration = null
-    memory = 256
-    memoryReservation = 128
-    mountPoints = null
-    name = null
-    portMappings = [
-      {
-        containerPort = 80
-        hostPort = 80
-        protocol = "tcp"
-      }
-    ]
-    privileged = null
-    readonlyRootFilesystem = false
-    repositoryCredentials = null
-    secrets = null
-    startTimeout = 30
-    stopTimeout = 30
-    systemControls = null
-    ulimits = null
-    user = null
-    volumesFrom = null
-    workingDirectory = null
-  }
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the container. Up to 255 characters ([a-z], [A-Z], [0-9], -, _ allowed)"
+  default     = null
+}
+
+variable "image" {
+  type        = string
+  description = "The image used to start the container. Images in the Docker Hub registry available by default"
+  default     = null
+}
+
+variable "memory" {
+  type        = number
+  description = "The amount of memory (in MiB) to allow the container to use. This is a hard limit, if the container attempts to exceed the container_memory, the container is killed. This field is optional for Fargate launch type and the total amount of container_memory of all containers in a task will need to be lower than the task memory value"
+  default     = 256
+}
+
+variable "memoryReservation" {
+  type        = number
+  description = "The amount of memory (in MiB) to reserve for the container. If container needs to exceed this threshold, it can do so up to the set container_memory hard limit"
+  default     = 128
+}
+
+variable "portMappings" {
+  type = list(object({
+    containerPort = number
+    hostPort      = number
+    protocol      = string
+  }))
+
+  description = "The port mappings to configure for the container. This is a list of maps. Each map should contain \"containerPort\", \"hostPort\", and \"protocol\", where \"protocol\" is one of \"tcp\" or \"udp\". If using containers in a task with the awsvpc or host network mode, the hostPort can either be left blank or set to the same value as the containerPort"
+
+  default = null
+}
+
+# https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_HealthCheck.html
+variable "healthCheck" {
+  type = object({
+    command     = list(string)
+    retries     = number
+    timeout     = number
+    interval    = number
+    startPeriod = number
+  })
+  description = "A map containing command (string), timeout, interval (duration in seconds), retries (1-10, number of times to retry before marking container unhealthy), and startPeriod (0-300, optional grace period to wait, in seconds, before failed healthchecks count toward retries)"
+  default     = null
+}
+
+variable "cpu" {
+  type        = number
+  description = "The number of cpu units to reserve for the container. This is optional for tasks using Fargate launch type and the total amount of container_cpu of all containers in a task will need to be lower than the task-level cpu value"
+  default     = null
+}
+
+variable "essential" {
+  type        = bool
+  description = "Determines whether all other containers in a task are stopped, if this container fails or stops for any reason. Due to how Terraform type casts booleans in json it is required to double quote this value"
+  default     = null
+}
+
+variable "entrypoint" {
+  type        = list(string)
+  description = "The entry point that is passed to the container"
+  default     = null
+}
+
+variable "command" {
+  type        = list(string)
+  description = "The command that is passed to the container"
+  default     = null
+}
+
+variable "workingDirectory" {
+  type        = string
+  description = "The working directory to run commands inside the container"
+  default     = null
 }
 
 variable "environment" {
   type = list(object({
-      name = string
-      value = string
-    }))
-  default = []
+    name  = string
+    value = string
+  }))
+  description = "The environment variables to pass to the container. This is a list of maps"
+  default     = null
 }
 
-variable "string_overrides" {
-  type = map(string)
-  default = {}
-}
-variable "port_mapping" {
+variable "secrets" {
   type = list(object({
-    containerPort = number
-    hostPort = number
-    protocol = string
+    name      = string
+    valueFrom = string
   }))
-  default = [
-      {
-        containerPort = 80
-        hostPort = 80
-        protocol = "tcp"
-      }
-    ]
+  description = "The secrets to pass to the container. This is a list of maps"
+  default     = null
+}
+
+variable "readonly_root_filesystem" {
+  type        = bool
+  description = "Determines whether a container is given read-only access to its root filesystem. Due to how Terraform type casts booleans in json it is required to double quote this value"
+  default     = false
+}
+
+# https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_LogConfiguration.html
+variable "logConfiguration" {
+  type = object({
+    logDriver = string
+    options   = map(string)
+    secretOptions = list(object({
+      name      = string
+      valueFrom = string
+    }))
+  })
+  description = "Log configuration options to send to a custom log driver for the container. For more details, see https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_LogConfiguration.html"
+  default     = null
+}
+
+# https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_FirelensConfiguration.html
+variable "firelensConfiguration" {
+  type = object({
+    type    = string
+    options = map(string)
+  })
+  description = "The FireLens configuration for the container. This is used to specify and configure a log router for container logs. For more details, see https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_FirelensConfiguration.html"
+  default     = null
+}
+
+variable "mountPoints" {
+  type = list(object({
+    containerPath = string
+    sourceVolume  = string
+  }))
+
+  description = "Container mount points. This is a list of maps, where each map should contain a `containerPath` and `sourceVolume`"
+  default     = null
+}
+
+variable "dnsServers" {
+  type        = list(string)
+  description = "Container DNS servers. This is a list of strings specifying the IP addresses of the DNS servers"
+  default     = null
+}
+
+variable "ulimits" {
+  type = list(object({
+    name      = string
+    hardLimit = number
+    softLimit = number
+  }))
+  description = "Container ulimit settings. This is a list of maps, where each map should contain \"name\", \"hardLimit\" and \"softLimit\""
+  default     = null
+}
+
+variable "repositoryCredentials" {
+  type        = map(string)
+  description = "Container repository credentials; required when using a private repo.  This map currently supports a single key; \"credentialsParameter\", which should be the ARN of a Secrets Manager's secret holding the credentials"
+  default     = null
+}
+
+variable "volumesFrom" {
+  type = list(object({
+    sourceContainer = string
+    readOnly        = bool
+  }))
+  description = "A list of VolumesFrom maps which contain \"sourceContainer\" (name of the container that has the volumes to mount) and \"readOnly\" (whether the container can write to the volume)"
+  default     = null
+}
+
+variable "links" {
+  type        = list(string)
+  description = "List of container names this container can communicate with without port mappings"
+  default     = null
+}
+
+variable "user" {
+  type        = string
+  description = "The user to run as inside the container. Can be any of these formats: user, user:group, uid, uid:gid, user:gid, uid:group"
+  default     = null
+}
+
+variable "dependsOn" {
+  type = list(object({
+    containerName = string
+    condition     = string
+  }))
+  description = "The dependencies defined for container startup and shutdown. A container can contain multiple dependencies. When a dependency is defined for container startup, for container shutdown it is reversed. The condition can be one of START, COMPLETE, SUCCESS or HEALTHY"
+  default     = null
+}
+
+variable "dockerLabels" {
+  type        = map(string)
+  description = "The configuration options to send to the `docker_labels`"
+  default     = null
+}
+
+variable "startTimeout" {
+  type        = number
+  description = "Time duration (in seconds) to wait before giving up on resolving dependencies for a container"
+  default     = 30
+}
+
+variable "stopTimeout" {
+  type        = number
+  description = "Time duration (in seconds) to wait before the container is forcefully killed if it doesn't exit normally on its own"
+  default     = 30
+}
+
+variable "privileged" {
+  type        = string
+  description = "When this variable is `true`, the container is given elevated privileges on the host container instance (similar to the root user). This parameter is not supported for Windows containers or tasks using the Fargate launch type. Due to how Terraform type casts booleans in json it is required to double quote this value"
+  default     = null
+}
+
+variable "systemControls" {
+  type        = list(map(string))
+  description = "A list of namespaced kernel parameters to set in the container, mapping to the --sysctl option to docker run. This is a list of maps: { namespace = \"\", value = \"\"}"
+  default     = null
 }
